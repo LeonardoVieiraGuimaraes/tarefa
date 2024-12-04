298aa917acb3475c3dccfa14c690124428252073
Leonardo Vieira Guimarães <leonardovieiraxy@hotmail.com>
2024-12-04 17:47:01 -0300

    Changes to be committed: 	modified:   Dockerfile.java 	modified:   docker-compose-app.yml 	modified:   docker-compose-mysql.yml

--- Dockerfile.java
+++ Dockerfile.java
@@ -7,8 +7,5 @@ WORKDIR /app
 # Copie o arquivo JAR da aplicação para o contêiner
 COPY target/*.jar /app/my-spring-boot-app.jar
 
-# Exponha a porta que a aplicação irá rodar
-EXPOSE 8085
-
 # Comando para executar a aplicação
 ENTRYPOINT ["java", "-jar", "my-spring-boot-app.jar"]
